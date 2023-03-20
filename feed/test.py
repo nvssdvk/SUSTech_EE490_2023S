8e1c153e2d4206490c6f2d6286cd722649f30a41
nvssdvk <et42828237@gmail.com>
2023-03-20 17:20:39 +0800

    Changes

--- feed/test.py
+++ feed/test.py
@@ -6,25 +6,43 @@ import pandas as pd
 import numpy as np
 
 
-def gradient_descent(f, x0, e):
-    # f: 目标函数
-    # x0: 初始值
-    # e: 收敛条件
-    alpha = 0.01  # 学习率
-    x = x0  # 当前值
-    while True:
-        grad = f(x) * 2 * x  # 计算梯度
-        x_new = x - alpha * grad  # 更新当前值
-        if abs(x_new - x) < e:  # 满足收敛条件
-            break  # 结束循环
-        else:
-            x = x_new  # 更新当前值
-    return x_new  # 返回最优值
-
-
-# 测试函数：f(x) = x**2 - 10*x + 25，最小值点为x=5
+def shrink(arr):
+    edge_min = 0
+    edge_max = 360 + edge_min
+    arr[arr > edge_max] -= 360
+    arr[arr < edge_min] += 360
+    if np.any(arr > edge_max) or np.any(arr < edge_min):
+        return shrink(arr)
+    else:
+        return arr
 
 
 if __name__ == "__main__":
-    a = 2
-    b = 8 / 2 ** 3
+    f = 32e9  # 工作频率
+    c = 3e8  # 光速
+    wl = c / f
+    print("wave length:{:.2f}mm".format(wl * 1e3))
+    k = 2 * np.pi / wl  # 波数
+    # h =
+    # feed = [-91.88 / 1e3, 0, 342.9 / 1e3]
+    feed = [0, 0, 0.17]
+    N = 40  # 反射面共有N*N个单元
+
+    dx = dy = 4.7 / 1e3
+    x_arr = np.arange(-N / 2 * dx, N / 2 * dx, dx)
+    y_arr = np.arange(-N / 2 * dy, N / 2 * dy, dy)
+    X, Y = np.meshgrid(x_arr, y_arr)
+
+    # 创建一个空的二维数组表示反射面上的相位分布
+    phi_arr = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            x = X[i, j]  # 获取当前单元的x坐标值
+            y = Y[i, j]  # 获取当前单元的y坐标值
+            phi_arr[i, j] = - k * np.sqrt((x - feed[0]) ** 2 + (y - feed[1]) ** 2 + feed[2] ** 2)  # 计算并赋值当前单元的相位值
+
+    phi_arr = shrink(phi_arr)
+    plt.figure()
+    plt.imshow(phi_arr, cmap='hot', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
--- feed/test_feed.py
+++ feed/test_feed.py
@@ -10,7 +10,7 @@ import seaborn as sns
 
 def plot_phase_distribution(phase_array):
     phase_data = pd.DataFrame(phase_array)
-    array_id = np.asarray(range(len(phase_array)), dtype=int)
+    array_id = np.asarray(range(len(phase_array)))
     df = pd.DataFrame(phase_data, index=array_id[::-1], columns=array_id)
 
     plt.figure()
@@ -20,18 +20,26 @@ def plot_phase_distribution(phase_array):
 
 
 def phase_distribution(wave_len, x, y, h):
+    def phase_normal(phase_array):
+        mask = phase_array > 180  # 创建一个布尔数组，表示哪些元素大于180
+        phase_array[mask] = np.subtract(phase_array[mask], 360)  # 对大于180的元素减去360
+        mask = phase_array < -180  # 创建一个布尔数组，表示哪些元素大于180
+        phase_array[mask] = np.add(phase_array[mask], 360)  # 对大于180的元素减去360
+        mask = (phase_array > 180) | (phase_array < -180)
+        cnt = np.count_nonzero(mask)
+        if cnt == 0:
+            return phase_array
+        else:  # 创建一个布尔数组，表示哪些元素大于180或小于-180
+            return phase_normal(phase_array)
+
     wave_num = 2 * np.pi / wave_len
+    # wave_num = 1 / wave_len
     r = np.zeros([len(x), len(y)])
     for i in range(len(x)):
         for j in range(len(y)):
             r[i, j] = np.sqrt(np.power(x[i], 2) + np.power(y[j], 2) + np.power(h, 2))
     phase_array = wave_num * r
-    for i in range(len(phase_array)):
-        for j in range(len(phase_array[i])):
-            if phase_array[i, j] > 180:
-                phase_array[i, j] -= 360
-            elif phase_array[i, j] < -180:
-                phase_array[i, j] += 360
+    phase_array = phase_normal(phase_array)
     return phase_array
 
 
@@ -92,6 +100,22 @@ def aperture_efficiency(wave_len, x, y, q):
 
 
 if __name__ == "__main__":
+    f = 10e9  # 工作频率为10GHz
+    c = 3e8  # 光速
+    k = 2 * np.pi * f / c  # 波数
+    x0 = 0  # 馈电天线的相位中心x坐标
+    y0 = 0  # 馈电天线的相位中心y坐标
+    z0 = 100  # 馈电天线的相位中心z坐标
+    N = 20  # 反射面共有20*20个单元
+    # 假设反射面边长为1米，每个单元边长为0.05米
+    dx = dy = 0.05
+    # 创建一个二维数组表示反射面上的x坐标
+    x_arr = np.arange(-N / 2 * dx, N / 2 * dx, dx)
+    # 创建一个二维数组表示反射面上的y坐标
+    y_arr = np.arange(-N / 2 * dy, N / 2 * dy, dy)
+    # 使用np.meshgrid函数将两个一维数组转换为二维网格数组
+    X, Y = np.meshgrid(x_arr, y_arr)
+
     wave_len = 0.3 / 10
     # x = np.arange(start=(-150 + 7.5) / 1e3, stop=150 / 1e3, step=15 / 1e3)
     # y = np.arange(start=(-150 + 7.5) / 1e3, stop=150 / 1e3, step=15 / 1e3)
@@ -100,7 +124,8 @@ if __name__ == "__main__":
     q = 6.5
     # h = aperture_efficiency(wave_len, x, y, q)
     # h = 0.27
-    h = 1.59
+    # h = 1.59
+    h = 100 * wave_len
     phase_array = phase_distribution(wave_len, x, y, h)
     # %%
     plot_phase_distribution(phase_array)
