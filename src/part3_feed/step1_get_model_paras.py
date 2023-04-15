bef47effe4c391c8eca77ab5527441f381b900d7
nvssdvk <et42828237@gmail.com>
2023-04-15 13:10:27 +0800

    Changes

--- src/part3_feed/step1_get_model_paras.py
+++ src/part3_feed/step1_get_model_paras.py
@@ -1,20 +1,21 @@
 import numpy as np
+import pandas as pd
 
 
 def f0(x):
-    out = np.power(np.sqrt(2 * x) - b / lam, 2) * (2 * x - 1) \
-          - np.power(gain / 2 / np.pi * np.sqrt(3 / 2 / np.pi) / np.sqrt(x) - a / lam, 2) \
+    out = np.power(np.sqrt(2 * x) - b / wl, 2) * (2 * x - 1) \
+          - np.power(gain / 2 / np.pi * np.sqrt(3 / 2 / np.pi) / np.sqrt(x) - a / wl, 2) \
           * (np.power(gain, 2) / 6 / np.power(np.pi, 3) / x - 1)
     return out
 
 
 def f1(x):
-    out = 2 * np.power(np.sqrt(2 * x) - b / lam, 2) \
-          + (np.sqrt(2 * x) - b / lam) * np.sqrt(2 / x) * (2 * x - 1) \
-          + (gain / 2 / np.pi * np.sqrt(3 / 2 / np.pi) / np.sqrt(x) - a / lam) * gain / 2 / np.pi \
+    out = 2 * np.power(np.sqrt(2 * x) - b / wl, 2) \
+          + (np.sqrt(2 * x) - b / wl) * np.sqrt(2 / x) * (2 * x - 1) \
+          + (gain / 2 / np.pi * np.sqrt(3 / 2 / np.pi) / np.sqrt(x) - a / wl) * gain / 2 / np.pi \
           * np.sqrt(3 / 2 / np.pi) / np.power(x, 3 / 2) \
           * (np.power(gain, 2) / 6 / np.power(np.pi, 3) / x - 1) \
-          + np.power(gain / 2 / np.pi * np.sqrt(3 / 2 / np.pi) / np.sqrt(x) - a / lam, 2) \
+          + np.power(gain / 2 / np.pi * np.sqrt(3 / 2 / np.pi) / np.sqrt(x) - a / wl, 2) \
           * np.power(gain, 2) / 6 / np.power(np.pi, 3) / np.power(x, 2)
     return out
 
@@ -34,19 +35,23 @@ def get_x(x0, max_iter=1000, loss_min=1e-5):
 
 
 if __name__ == "__main__":
-    fc = 11
-    lam = 0.3 / 11
+    wl = 3e8 / 10e9
     gain = np.power(10, 22.6 / 10)
     a = 0.9 * 25.4 / 1e3
     b = 0.4 * 25.4 / 1e3
     x0 = gain / (2 * np.pi * np.sqrt(2 * np.pi))
     x, loss = get_x(x0)
-    pe = x * lam
-    ph = gain ** 2 / 8 / np.pi ** 3 / x * lam
-    a1 = np.sqrt(3 * lam * ph)
-    b1 = np.sqrt(2 * lam * pe)
+    pe = x * wl
+    ph = gain ** 2 / 8 / np.pi ** 3 / x * wl
+    a1 = np.sqrt(3 * wl * ph)
+    b1 = np.sqrt(2 * wl * pe)
     lh = (b1 - b) * np.sqrt(np.power(pe / b1, 2) - 1 / 4)
     le = (a1 - a) * np.sqrt(np.power(ph / a1, 2) - 1 / 4)
 
     taper_h = np.arctan(a1 / 2 / ph) / np.pi * 180
     taper_e = np.arctan(b1 / 2 / pe) / np.pi * 180
+
+    df_name = ['wg_a', 'wg_b', 'wg_h', 'a', 'b', 'h', 'thickness']
+    df_data = np.asarray([a * 1e3, b * 1e3, wl / 4 * 1e3, a1 * 1e3, b1 * 1e3, lh * 1e3, 2]).reshape(1, -1)
+    df = pd.DataFrame(columns=df_name, data=df_data)
+    df.to_csv(r'../../data/dataset/feed_horn_para.csv', encoding='utf-8', index=False)
--- src/part3_feed/step3_feed_modeling.py
+++ src/part3_feed/step3_feed_modeling.py
@@ -1,3 +1,229 @@
+import os
+import time
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+import cst.interface
+
+line_break = '\n'
+
+
+def add_to_history(modeler, header, vbacode):
+    res = modeler.add_to_history(header, vbacode)
+    return res
+
+
+def set_units(modeler):
+    header = "set units"
+    vbacode = [
+        'With Units',
+        '    .SetUnit "Length", "mm"',
+        '    .SetUnit "Frequency", "GHz"',
+        '    .SetUnit "Voltage", "V"',
+        '    .SetUnit "Resistance", "Ohm"',
+        '    .SetUnit "Inductance", "nH"',
+        '    .SetUnit "Temperature",  "degC"',
+        '    .SetUnit "Time", "ns"',
+        '    .SetUnit "Current", "A"',
+        '    .SetUnit "Conductance", "S"',
+        '    .SetUnit "Capacitance", "pF"',
+        'End With'
+    ]
+    vbacode = line_break.join(vbacode)
+    add_to_history(modeler, header, vbacode)
+
+
+def set_freq_range(modeler, f_min, f_max):
+    header = "set frequency range"
+    vbacode = 'Solver.FrequencyRange "{:.1f}", "{:.1f}"'.format(f_min, f_max)
+    add_to_history(modeler, header, vbacode)
+
+
+def set_background(modeler, zmax=0.0):
+    header = "define background"
+    vbacode = [
+        'With Background',
+        '     .Type "Normal"',
+        '     .Epsilon "1.0"',
+        '     .Mu "1.0"',
+        '     .Rho "1.204"',
+        '     .ThermalType "Normal"',
+        '     .ThermalConductivity "0.026"',
+        '      .SpecificHeat "1005", "J/K/kg"',
+        '     .XminSpace "0.0"',
+        '     .XmaxSpace "0.0"',
+        '     .YminSpace "0.0"',
+        '     .YmaxSpace "0.0"',
+        '     .ZminSpace "0.0"',
+        '     .ZmaxSpace "{:.2f}"'.format(zmax),
+        'End With',
+    ]
+    vbacode = line_break.join(vbacode)
+    add_to_history(modeler, header, vbacode)
+
+
+def set_boundaries(modeler):
+    header = "define boundaries"
+    vbacode = [
+        'With Boundary',
+        '     .Xmin "expanded open"',
+        '     .Xmax "expanded open"',
+        '     .Ymin "expanded open"',
+        '     .Ymax "expanded open"',
+        '     .Zmin "expanded open"',
+        '     .Zmax "expanded open"',
+        '     .Xsymmetry "none"',
+        '     .Ysymmetry "none"',
+        '     .Zsymmetry "none"',
+        '     .ApplyInAllDirections "False"',
+        '     .OpenAddSpaceFactor "0.5"',
+        '     .XPeriodicShift "0.0"',
+        '     .YPeriodicShift "0.0"',
+        '     .ZPeriodicShift "0.0"',
+        '     .PeriodicUseConstantAngles "False"',
+        '     .SetPeriodicBoundaryAngles "theta", "phi"',
+        '     .SetPeriodicBoundaryAnglesDirection "inward"',
+        '     .UnitCellFitToBoundingBox "True"',
+        '     .UnitCellDs1 "0.0"',
+        '     .UnitCellDs2 "0.0"',
+        '     .UnitCellAngle "90.0"',
+        'End With',
+    ]
+    vbacode = line_break.join(vbacode)
+    add_to_history(modeler, header, vbacode)
+
+
+def zoom(modeler):
+    header = "zoom"
+    vbacode = 'Plot.ZoomToStructure'
+    add_to_history(modeler, header, vbacode)
+
+
+def modeling_horn_main(modeler, wg_a, wg_b, wg_h, a, b, h, thickness):
+    header = 'modeling'
+    vbacode = [
+        'With Polygon3D ',
+        '     .Reset ',
+        '     .Version 10 ',
+        '     .Name "aperture_top" ',
+        '     .Curve "curve1" ',
+        '     .Point "{:.2f}", "{:.2f}", "{:.2f}" '.format(-a / 2, -b / 2, wg_h + h),
+        '     .Point "{:.2f}", "{:.2f}", "{:.2f}" '.format(a / 2, - b / 2, wg_h + h),
+        '     .Point "{:.2f}", "{:.2f}", "{:.2f}" '.format(a / 2, b / 2, wg_h + h),
+        '     .Point "{:.2f}", "{:.2f}","{:.2f}" '.format(-a / 2, b / 2, wg_h + h),
+        '     .Point "{:.2f}", "{:.2f}","{:.2f}" '.format(-a / 2, - b / 2, wg_h + h),
+        '     .Create ',
+        'End With',
+        '',
+        '',
+        'With Polygon3D ',
+        '     .Reset ',
+        '     .Version 10 ',
+        '     .Name "aperture_bottom" ',
+        '     .Curve "curve1" ',
+        '     .Point "{:.2f}", "{:.2f}", "{:.2f}" '.format(-wg_a / 2, -wg_b / 2, wg_h),
+        '     .Point "{:.2f}", "{:.2f}", "{:.2f}" '.format(wg_a / 2, - wg_b / 2, wg_h),
+        '     .Point "{:.2f}", "{:.2f}", "{:.2f}" '.format(wg_a / 2, wg_b / 2, wg_h),
+        '     .Point "{:.2f}", "{:.2f}","{:.2f}" '.format(-wg_a / 2, wg_b / 2, wg_h),
+        '     .Point "{:.2f}", "{:.2f}","{:.2f}" '.format(-wg_a / 2, - wg_b / 2, wg_h),
+        '     .Create ',
+        'End With',
+        '',
+        '',
+        'With CoverCurve',
+        '     .Reset ',
+        '     .Name "aperture_bottom" ',
+        '     .Component "component1" ',
+        '     .Material "PEC" ',
+        '     .Curve "curve1:aperture_bottom" ',
+        '     .DeleteCurve "True" ',
+        '     .Create',
+        'End With',
+        '',
+        '',
+        'With CoverCurve',
+        '     .Reset ',
+        '     .Name "aperture_top" ',
+        '     .Component "component1" ',
+        '     .Material "PEC" ',
+        '     .Curve "curve1:aperture_top" ',
+        '     .DeleteCurve "True" ',
+        '     .Create',
+        'End With',
+        '',
+        '',
+        'Pick.PickFaceFromPoint  "{:s}", "{:.2f}", "{:.2f}", "{:.2f}"'.format(
+            "component1:aperture_bottom", 0, 0, wg_h),
+        '',
+        '',
+        'Pick.PickFaceFromPoint  "{:s}", "{:.2f}", "{:.2f}", "{:.2f}"'.format(
+            "component1:aperture_top", 0, 0, wg_h + h),
+        '',
+        '',
+        'With Loft ',
+        '     .Reset ',
+        '     .Name "aperture" ',
+        '     .Component "component1" ',
+        '     .Material "PEC" ',
+        '     .Tangency "0.0" ',
+        '     .Minimizetwist "true" ',
+        '     .CreateNew ',
+        'End With',
+        '',
+        '',
+        'Solid.Add "{:s}", "{:s}"'.format("component1:aperture", "component1:aperture_bottom"),
+        '',
+        '',
+        'Solid.Add "{:s}", "{:s}"'.format("component1:aperture", "component1:aperture_top"),
+        '',
+        '',
+        'With Brick',
+        '     .Reset ',
+        '     .Name "waveguide" ',
+        '     .Component "component1" ',
+        '     .Material "PEC" ',
+        '     .Xrange "{:.2f}", "{:.2f}" '.format(-wg_a / 2, wg_a / 2),
+        '     .Yrange "{:.2f}", "{:.2f}" '.format(-wg_b / 2, wg_b / 2),
+        '     .Zrange "{:.2f}", "{:.2f}" '.format(0, wg_h),
+        '     .Create',
+        'End With',
+        '',
+        '',
+        'Solid.Add "{:s}", "{:s}"'.format("component1:aperture", "component1:waveguide"),
+        '',
+        '',
+        'Solid.Rename "component1:aperture", "horn"',
+        '',
+        '',
+        'Pick.PickFaceFromPoint  "{:s}", "{:.2f}", "{:.2f}", "{:.2f}"'.format("component1:horn", 0, 0, 0),
+        '',
+        '',
+        'Pick.PickFaceFromPoint  "{:s}", "{:.2f}", "{:.2f}", "{:.2f}"'.format("component1:horn", 0, 0, wg_h + h),
+        '',
+        '',
+        'Solid.ShellAdvanced "component1:horn", "Inside", "{:.2f}", "True"'.format(thickness),
+    ]
+    vbacode = line_break.join(vbacode)
+    add_to_history(modeler, header, vbacode)
+
 
 if __name__ == "__main__":
-    pass
+    data = pd.read_csv(r'../../data/dataset/feed_horn_para.csv').values
+    wg_a, wg_b, wg_h, a, b, h, thickness = data.squeeze()
+
+    my_project_name = f'feed_horn_10GHz.cst'
+    my_project_path = os.path.abspath(f'../../cst/{my_project_name}')
+    my_de = cst.interface.DesignEnvironment()
+    my_mws = my_de.new_mws()
+    my_mws.save(my_project_path, include_results=True)
+    my_modeler = my_mws.modeler
+
+    set_units(my_modeler)
+    set_freq_range(my_modeler, 6, 14)
+    set_background(my_modeler)
+
+    modeling_horn_main(my_modeler, wg_a, wg_b, wg_h, a, b, h, thickness)
+    zoom(my_modeler)
+
+    my_mws.save()
