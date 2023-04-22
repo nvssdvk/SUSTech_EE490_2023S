9bf23b369e39a026a0c31cf2014b161effc7571d
nvssdvk <et42828237@gmail.com>
2023-04-22 12:59:50 +0800

    Changes

--- src/part5_system_analysis/step2_array_theory.py
+++ src/part5_system_analysis/step2_array_theory.py
@@ -15,29 +15,33 @@ def vector_normalize(vector):
     return out
 
 
-def cal_element_pattern(qe, uv, bv, theta):
+def cal_element_pattern(qe, uv, ov, theta):
     element_pattern = np.zeros_like(theta, dtype=complex)
     for index in range(len(theta)):
-        # temp_1 = np.exp(1j * k * vector_normalize(uv * bv[index]))
-        temp_1 = np.exp(1j * k * (np.dot(uv, bv[index])))
+        # temp_1 = np.exp(1j * k * vector_normalize(uv * ov[index]))
+        temp_1 = np.exp(1j * k * (np.dot(uv, ov[index])))
         temp_2 = np.sign(np.cos(theta[index])) * (np.abs(np.cos(theta[index]))) ** qe
         element_pattern[index] = temp_1 * temp_2
     return element_pattern
 
 
-def cal_excitation(qf, qe, uv, fv, bv, phase, theta):
+def cal_excitation(qf, qe, uv, fv, ov, bv, phase, theta):
     illumination = np.zeros_like(theta, dtype=complex)
     r = np.linalg.norm(fv - uv)
     temp_2 = np.exp(-1j * k * r)
     temp_4 = np.exp(1j * phase)
     for index in range(len(theta)):
-        bv_sub = bv[index]
+        ov_sub = ov[index]
+        # if np.linalg.norm(uv) != 0:
+        #     theta_e = np.arccos(np.dot(uv, bv_sub) / (np.linalg.norm(uv) * np.linalg.norm(bv_sub)))
+        # else:
+        #     theta_e = np.arccos(bv_sub[2] / np.linalg.norm(bv_sub))
+        # theta_f = np.arccos(bv_sub[2] / np.linalg.norm(bv_sub))
         if np.linalg.norm(uv) != 0:
-            theta_e = np.arccos(np.dot(uv, bv_sub) / (np.linalg.norm(uv) * np.linalg.norm(bv_sub)))
+            theta_e = np.arccos(np.dot(uv, bv) / (np.linalg.norm(uv) * np.linalg.norm(bv)))
         else:
-            theta_e = np.arccos(bv_sub[2] / np.linalg.norm(bv_sub))
-        theta_f = np.arccos(bv_sub[2] / np.linalg.norm(bv_sub))
-        # theta_f = np.arccos(np.dot(-fv, bv_sub) / (np.linalg.norm(fv) * np.linalg.norm(bv_sub)))
+            theta_e = np.arccos(bv[2] / np.linalg.norm(bv))
+        theta_f = np.arccos(np.dot(bv, fv) / (np.linalg.norm(fv) * np.linalg.norm(bv)))
         temp_1 = np.sign(np.cos(theta_f)) * (np.abs(np.cos(theta_f))) ** qf
         temp_3 = np.sign(np.cos(theta_e)) * (np.abs(np.cos(theta_e))) ** qe
         illumination[index] = temp_1 * temp_2 * temp_3 * temp_4
@@ -102,10 +106,11 @@ if __name__ == "__main__":
     phi = 90
     theta = np.linspace(-180, 180, 361, dtype=int)
     uv = np.stack((x, y, z), axis=2)
-    bv = np.zeros([len(theta), 3])
-    bv[:, 0] = np.sin(np.deg2rad(theta)) * np.cos(np.deg2rad(phi))
-    bv[:, 1] = np.sin(np.deg2rad(theta)) * np.sin(np.deg2rad(phi))
-    bv[:, 2] = np.cos(np.deg2rad(theta))
+    bv = np.asarray([0, 0, 1])
+    ov = np.zeros([len(theta), 3])
+    ov[:, 0] = np.sin(np.deg2rad(theta)) * np.cos(np.deg2rad(phi))
+    ov[:, 1] = np.sin(np.deg2rad(theta)) * np.sin(np.deg2rad(phi))
+    ov[:, 2] = np.cos(np.deg2rad(theta))
     fv = np.asarray([0, 0, 9.5 * wl])
 
     aperture_pattern = np.zeros(len(theta), dtype=complex)
@@ -114,8 +119,8 @@ if __name__ == "__main__":
     illumination_array = np.zeros([num, num, len(theta)], dtype=complex)
     for i in range(num):
         for j in range(num):
-            pattern = cal_element_pattern(qe=qe, uv=uv[i, j], bv=bv, theta=np.deg2rad(theta))
-            illumination = cal_excitation(qe=qe, qf=qf, uv=uv[i, j], fv=fv, bv=bv,
+            pattern = cal_element_pattern(qe=qe, uv=uv[i, j], ov=ov, theta=np.deg2rad(theta))
+            illumination = cal_excitation(qe=qe, qf=qf, uv=uv[i, j], fv=fv, ov=ov, bv=bv,
                                           phase=np.deg2rad(aperture_phase[i, j]), theta=np.deg2rad(theta))
             # pattern = np.power(10, pattern/10)
             aperture_pattern_array[i, j] = pattern * illumination
